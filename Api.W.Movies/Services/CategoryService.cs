eed926bc1170e1720ca4b7504a12b07c7fe55e81
GustavoAcosta2025 <gustavoacosta1127965@correo.itm.edu.co>
2025-11-29 16:09:40 -0500

    GetCategory de la clase 4 (Buscar por ID)

--- Api.W.Movies/Services/CategoryService.cs
+++ Api.W.Movies/Services/CategoryService.cs
@@ -16,39 +16,45 @@ namespace Api.W.Movies.Services
             _mapper = mapper;
         }
 
-        public Task<bool> CategoryExistsByIdAsync(int id)
+        public async Task<bool> CategoryExistsByIdAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> CategoryExistsByNameAsync(string name)
+        public async Task<bool> CategoryExistsByNameAsync(string name)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> CreateCategoryAsync(Category category)
+        public async Task<bool> CreateCategoryAsync(Category category)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> DeleteCategoryAsync(int id)
+        public async Task<bool> DeleteCategoryAsync(int id)
         {
             throw new NotImplementedException();
         }
 
         public async Task<ICollection<CategoryDto>> GetCategoriesAsync()
         {
+            //Obtener las categorias del repositorio
             var categories = await _categoryRepository.GetCategoriesAsync(); // Solo estoy llamando el metodo desde la capa Repository
 
+            //Mapear toda la colección de una vez
             return _mapper.Map<ICollection<CategoryDto>>(categories); //Mapeo la lista de categorias a una lista de categorias Dto
         }
 
-        public Task<CategoryDto> GetCategoryAsync(int id)
+        public async Task<CategoryDto> GetCategoryAsync(int id)
         {
-            throw new NotImplementedException();
+            //Obtener la categoria del repositorio
+            var category = await _categoryRepository.GetCategoryAsync(id);
+
+            //Mapear el objeto de una vez
+            return _mapper.Map<CategoryDto>(category);
         }
 
-        public Task<bool> UpdateCategoryAsync(Category category)
+        public async Task<bool> UpdateCategoryAsync(Category category)
         {
             throw new NotImplementedException();
         }
---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO DBO.Categories (Name, CreatedDate)
+VALUES (N'Thriller', SYSDATETIME()),
+       (N'Comedy', SYSDATETIME()),
+       (N'Action', SYSDATETIME());
+
+select *from Categories
