a0219d3fb43efe6267f6646fd5a89206f6cc6284
GustavoAcosta2025 <gustavoacosta1127965@correo.itm.edu.co>
2025-11-28 22:27:39 -0500

    Automappers y creación de Dtos

---
+++ Api.W.Movies/DAL/Models/Dtos/CategoryCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.W.Movies.DAL.Models.Dtos
+{
+    public class CategoryCreateDto
+    {
+        [Required(ErrorMessage = "El nombre de la categoria es obligatorio.")]
+        [MaxLength(100, ErrorMessage ="El número máximo de caracteres es de 100.")]
+        public string Name { get; set; }
+    }
+}
---
+++ Api.W.Movies/MoviesMapper/Mappers.cs
@@ -0,0 +1,16 @@
+﻿using Api.W.Movies.DAL.Models;
+using Api.W.Movies.DAL.Models.Dtos;
+using AutoMapper;
+
+namespace Api.W.Movies.MoviesMapper
+{
+    public class Mappers : Profile
+    {
+        public Mappers()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+
+            CreateMap<Category, CategoryCreateDto>().ReverseMap();
+        }
+    }
+}
